513dbde8a55cb06a68f3e66a0aa3cc8710e7e78b
Vlad Alex <vladalex997@gmail.com>
2021-09-27 14:35:27 +0300

    files commit

---
+++ proiectImagini/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace proiectImagini
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.labelImagine = new System.Windows.Forms.Label();
+            this.buttonUpload = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBoxCod = new System.Windows.Forms.TextBox();
+            this.labelCode = new System.Windows.Forms.Label();
+            this.labelError = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(192, 31);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(100, 22);
+            this.textBoxId.TabIndex = 0;
+            // 
+            // labelImagine
+            // 
+            this.labelImagine.AutoSize = true;
+            this.labelImagine.Location = new System.Drawing.Point(100, 31);
+            this.labelImagine.Name = "labelImagine";
+            this.labelImagine.Size = new System.Drawing.Size(72, 17);
+            this.labelImagine.TabIndex = 1;
+            this.labelImagine.Text = "Id Imagine";
+            // 
+            // buttonUpload
+            // 
+            this.buttonUpload.Location = new System.Drawing.Point(333, 23);
+            this.buttonUpload.Name = "buttonUpload";
+            this.buttonUpload.Size = new System.Drawing.Size(84, 38);
+            this.buttonUpload.TabIndex = 2;
+            this.buttonUpload.Text = "Upload";
+            this.buttonUpload.UseVisualStyleBackColor = true;
+            this.buttonUpload.Click += new System.EventHandler(this.buttonUpload_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(48, 141);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(2438, 1081);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBoxCod
+            // 
+            this.textBoxCod.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxCod.Location = new System.Drawing.Point(968, 23);
+            this.textBoxCod.Name = "textBoxCod";
+            this.textBoxCod.Size = new System.Drawing.Size(767, 76);
+            this.textBoxCod.TabIndex = 4;
+            // 
+            // labelCode
+            // 
+            this.labelCode.AutoSize = true;
+            this.labelCode.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCode.Location = new System.Drawing.Point(773, 25);
+            this.labelCode.Name = "labelCode";
+            this.labelCode.Size = new System.Drawing.Size(153, 68);
+            this.labelCode.TabIndex = 5;
+            this.labelCode.Text = "Cod:";
+            // 
+            // labelError
+            // 
+            this.labelError.AutoSize = true;
+            this.labelError.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelError.ForeColor = System.Drawing.Color.Red;
+            this.labelError.Location = new System.Drawing.Point(97, 96);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(303, 32);
+            this.labelError.TabIndex = 6;
+            this.labelError.Text = "Eroare Codul nu exista";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2612, 1325);
+            this.Controls.Add(this.labelError);
+            this.Controls.Add(this.labelCode);
+            this.Controls.Add(this.textBoxCod);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonUpload);
+            this.Controls.Add(this.labelImagine);
+            this.Controls.Add(this.textBoxId);
+            this.Name = "Form1";
+            this.Text = "PLC IMAGES";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.Label labelImagine;
+        private System.Windows.Forms.Button buttonUpload;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBoxCod;
+        private System.Windows.Forms.Label labelCode;
+        private System.Windows.Forms.Label labelError;
+    }
+}
+
---
+++ proiectImagini/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+
+
+namespace proiectImagini
+{
+    public partial class Form1 : Form
+    {
+        public string Cod;
+        public Timer timer1;
+        public Timer timer2;
+        public Image current;
+        public Image previous;
+        public Image Eroare=new Bitmap(proiectImagini.Properties.Resources.Eroare);
+        public Form1()
+        {
+            InitializeComponent();
+            labelImagine.Visible = false;
+            textBoxId.Visible = false;
+            buttonUpload.Visible = false;
+            labelError.Visible = false;
+           // GetImage();
+            InitTimer1();
+            InitTimer2();
+        }
+
+        private void buttonUpload_Click(object sender, EventArgs e)
+        {
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK&&int.TryParse(textBoxId.Text, out int docu))
+            {
+                string filename = openFileDialog1.FileName;
+                string contentType = "image/jpeg";
+                byte[] bytes = File.ReadAllBytes(filename);
+                Database databaseObject = new Database();
+                string query = "UPDATE PLCImages set Nume=@Nume, ContentType=@ContentType, DataImagine=@DataImagine WHERE Id=@Id";
+                SqlCommand mycommand = new SqlCommand(query, databaseObject.myConnection);
+                mycommand.Parameters.AddWithValue("@Nume",filename);
+                mycommand.Parameters.AddWithValue("@ContentType", contentType);
+                mycommand.Parameters.AddWithValue("DataImagine",bytes);
+                mycommand.Parameters.AddWithValue("@Id",int.Parse(textBoxId.Text));
+
+                databaseObject.OpenConnection();
+                var result = mycommand.ExecuteNonQuery();
+                databaseObject.CloseConnection();
+
+                MessageBox.Show("Incarcare imagine reusita");
+
+            }
+            else
+            {
+                MessageBox.Show("Selectati fisier si scrieti Id Corect");
+            }
+
+          
+
+
+        }
+
+        public void InitTimer1()
+        {
+            timer1 = new Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 100; // nr sec in mili
+            timer1.Start();
+
+
+
+
+        }
+
+        public void InitTimer2()
+        {
+            timer2 = new Timer();
+            timer2.Tick += new EventHandler(timer2_Tick);
+            timer2.Interval = 1500; // nr sec in mili
+            timer2.Start();
+
+
+
+
+        }
+
+     
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (textBoxCod.Text != "")
+            {
+                GetImage();
+                previous = current;
+                Display();
+            }
+           
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            textBoxCod.Text = "";
+
+
+
+        }
+
+        public void Display()
+        {
+            pictureBox1.Image = previous;
+        }
+
+        public void GetImage()
+        {
+            try
+            {
+                Database databaseObject = new Database();
+                databaseObject.OpenConnection();
+
+                Cod = textBoxCod.Text;
+
+                string query = "SELECT DataImagine from PLCImages WHERE Cod=@Cod";
+                SqlCommand mycommand = new SqlCommand(query, databaseObject.myConnection);
+                mycommand.Parameters.AddWithValue("@Cod", Cod);
+
+                SqlDataReader reader = mycommand.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    current = ByteArrayToImage((byte[])(reader.GetValue(0)));
+
+                    labelError.Visible = false;
+                }
+                else
+                {
+
+                    
+
+                    current = Eroare;
+                 //   labelError.Visible = true;
+
+                }
+                reader.Close();
+
+
+                databaseObject.CloseConnection();
+
+               
+
+            }
+            catch (Exception)
+            {
+             //   labelError.Visible = true;
+            }
+            
+
+        }
+
+        public static Image ByteArrayToImage(byte[] byteArrayIn)
+        {
+            using (MemoryStream ms = new MemoryStream(byteArrayIn))
+            {
+                Image returnImage = Image.FromStream(ms);
+                return returnImage;
+            }
+        }
+    }
+}
